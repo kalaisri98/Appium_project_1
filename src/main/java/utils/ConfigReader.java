f6b1e0cc0c7c539764ea46e86a0915252cddd0e9
navee <navee@INBOOK_X1_SLIM>
2025-07-27 10:45:52 +0530

    final commit

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,22 +1,29 @@
 package utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
+    private static final Logger logger = LogManager.getLogger(ConfigReader.class);
     private static Properties prop = new Properties();
 
     static {
         try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
             prop.load(input);
+            logger.info("Configuration file loaded successfully.");
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Failed to load config.properties", e);
             throw new RuntimeException("Failed to load config.properties", e);
         }
     }
 
     public static String get(String key) {
-        return prop.getProperty(key);
+        String value = prop.getProperty(key);
+        logger.debug("Reading config key: " + key + ", value: " + value);
+        return value;
     }
 }
--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,12 +2,15 @@ package utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DriverFactory {
+    private static final Logger logger = LogManager.getLogger(DriverFactory.class);
     private static AppiumDriver driver;
 
     public static AppiumDriver getDriver() {
@@ -15,17 +18,23 @@ public class DriverFactory {
     }
 
     public static void initDriver() throws MalformedURLException {
+        logger.info("Initializing Appium driver...");
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", ConfigReader.get("platformName"));
         caps.setCapability("deviceName", ConfigReader.get("deviceName"));
         caps.setCapability("app", ConfigReader.get("appPath"));
+
         String serverUrl = ConfigReader.get("appiumServerUrl");
-        driver = new AndroidDriver(new URL(serverUrl), caps);;
+        logger.debug("Connecting to Appium server at: " + serverUrl);
+        driver = new AndroidDriver(new URL(serverUrl), caps);
+        logger.info("Appium driver initialized successfully.");
     }
 
     public static void quitDriver() {
         if (driver != null) {
+            logger.info("Quitting Appium driver...");
             driver.quit();
+            logger.info("Appium driver session ended.");
         }
     }
 }
